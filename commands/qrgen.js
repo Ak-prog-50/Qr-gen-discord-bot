e08c026a7006d202b08703ba68de6f5de06e9970
Ak <akalankapathirage22@gmail.com>
2021-12-21 19:03:59 +0530

    implement api request sending using needle

--- commands/qrgen.js
+++ commands/qrgen.js
@@ -1,12 +1,21 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const needle = require('needle');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('qrgen')
 		.setDescription('Replies with a Generated QR Code!')
-        .addStringOption(option => option.setName('input').setDescription('Enter a string')),
-	async execute(interaction) {
+        .addStringOption(option => option.setName('query').setDescription('Enter a query!')),
+	execute(interaction) {
         const qrQuery = interaction.options.getString('input');
-		await interaction.reply(qrQuery);
+        getQr(qrQuery).then(async res => {
+            console.log(res.body)
+            await interaction.reply(res.body.toString())
+        })
+		;
 	},
 };
+
+const getQr = async (query) => {
+    return await needle('get', 'https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=Hello')
+}
