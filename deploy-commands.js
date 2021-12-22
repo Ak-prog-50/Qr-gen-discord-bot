c2b1501e570e05dfc3ea2b95a8f5764dc97773c1
Ak <akalankapathirage22@gmail.com>
2021-12-22 17:03:10 +0530

    made application commands available globally

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId } = require('./config-local.json');
+const { clientId } = require('./config-local.json');
 
 require('dotenv').config()
 
@@ -16,6 +16,6 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(process.env.QR_BOT_TOKEN);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
